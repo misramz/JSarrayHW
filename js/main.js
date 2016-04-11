e3c5da1aa1e15286e6e6140a454df3246233dae7
Ramatoulie Sanyang <misramz@yahoo.com>
2016-04-11 11:03:22 -0400

    review with tim

--- js/main.js
+++ js/main.js
@@ -1,9 +1,48 @@
+
+// 1. Show me how to calculate the average price of all items.
+
+
+// To get a sum of all prices, we first need all prices
+// This `map` will give us an array of all prices
 var allPrices = items.map( function (item) {
-  Return item.price;
+  return item.price;
 });
 
-console.log(allPrices);
-
-function addUpItemsPrices (prev, curr) {
+// Now that I have all the prices, we can `reduce` them down to
+// a single value, which is the sum of all prices
+var priceSum = allPrices.reduce(function (prev, curr) {
   return prev + curr;
+});
+
+// Once we have the sum, we can divide by the number (length) of
+// items we have
+var avg = priceSum / items.length;
+
+// Let's now round that to 2 decimals
+var final = avg.toFixed(2);
+
+// Finally, lets build our answer string
+var answer1 = "The average price is $" + final;
+
+// Find my "answer" area in the DOM
+var answer1Area = document.querySelector('#answer1');
+
+// Put our answer on the page
+answer1Area.innerHTML = answer1;
+
+
+// 2.
+
+var items = [14, 15, 16, 17, 18];
+
+
+
+// 3.
+
+
+
+
+// 4.
+var woodyItems = items.filter(function(item) {
+  return item.materials 
 }
